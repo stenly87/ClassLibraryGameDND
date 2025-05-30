9c5152683d90613a8c8b6a2af262006c3d81aa67
Denoos <148026464+Denoos@users.noreply.github.com>
2025-05-31 00:00:29 +1000

    Бфл реализован класс кубика, который наверное работает, слабым местом является regex...

--- ClassLibraryGameDND/Models/OtherModels/Dice.cs
+++ ClassLibraryGameDND/Models/OtherModels/Dice.cs
@@ -2,18 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ClassLibraryGameDND.Models.OtherModels
 {
     public class Dice
     {
-        /*
-         *      Здесь будет обработка работы кубиков
-         * 
-         * Типо метод принимает в себя строчку, например "1d20", делает валидацию на то, что это реально кубик, а не ранодомная sql инъекция
-         * 
-         * Дальше он сплитует строку и делает нормальный бросок кубика нужное количество раз, после чего кладывает значение и возвращает int value
-         */
+        public int Rolling(string dice)
+        {
+            if (InvalidnessCheck(dice))
+                throw new ArgumentException("Ошибка в валидности переданого кубика!");
+
+            var diceValues = dice.Split('d', StringSplitOptions.RemoveEmptyEntries);
+            var times = int.Parse(diceValues[0]);
+            var diceType = int.Parse(diceValues[1]);
+
+            var rnd = new Random();
+            var result = 0;
+
+            for (int i = 0; i < times; i++)
+                result += rnd.Next(++diceType);
+
+            return result;
+        }
+
+        private bool InvalidnessCheck(string dice)
+        {
+            var result = true;
+
+            var regex = new Regex("^(?!\\d+$)(([1-9]\\d*)?[Dd]?[1-9]\\d*( ?[+-] ?)?)+$");
+            if (regex.IsMatch(dice))
+                result = false;
+
+            var diceValue = int.Parse(dice.Split('d', StringSplitOptions.RemoveEmptyEntries)[1]);
+            int[] validValues = [4, 6, 8, 10, 12, 20];
+            if (!validValues.Contains(diceValue))
+                result = false;
+
+            return result;
+        }
     }
 }
