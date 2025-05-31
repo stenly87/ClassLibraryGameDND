ea3e632bbb1a8cece6ed1215fe352f2faa6dc8b7
skazo4nik262 <skazo4nik@DESKTOP-81KEU8S>
2025-05-31 18:47:58 +1000

    PetParser remake

--- ClassLibraryGameDND/Models/OtherModels/PetParser.cs
+++ ClassLibraryGameDND/Models/OtherModels/PetParser.cs
@@ -2,16 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
 namespace ClassLibraryGameDND.Models.OtherModels
 {
     public static class PetParser
     {
+        //{"Character":666, "AC":27,"BAB":10,"BaseDamage":"1d8","CHA":10,"CON":18,"CritHitMult":2,"DEX":20,"DamageBonus":"1d6","Fort":14,"GoodEvil":5,"INT":9,"LawChaos":1,"MaxHP":160,"Name":"\u0417\u0438\u043c\u043d\u0438\u0439 \u0432\u043e\u043b\u043a","Refl":12,"STR":24,"WIS":14,"Will":12}
         public static Pet PetParse(string pet)
         {
+            var jObj = JsonObject.Parse(pet);
             Pet localPet = new Pet();
-            var petParts = pet.Split(',');
+            try
+            {
+                localPet.Character = new Character { ID = (int)jObj["Character"] };
+                localPet.AC = (int)jObj["AC"];
+                localPet.BAB = (int)jObj["BAB"];
+
+                localPet.BaseDamage = Dice.Rolling((string)jObj["BaseDamage"]);
+                localPet.CHA = (int)jObj["CHA"];
+                localPet.CON = (int)jObj["CON"];
+                localPet.CritHitMult = (int)jObj["CritHitMult"];
+                localPet.DEX = (int)jObj["DEX"];
+                localPet.DamageBonus = Dice.Rolling((string)jObj["DamageBonus"]);
+                localPet.Fort = (int)jObj["Fort"];
+                localPet.GoodEvil = (int)jObj["GoodEvil"];
+                localPet.INT = (int)jObj["INT"];
+                localPet.LawCHaos = (int)jObj["LawChaos"];
+                localPet.MaxHP = (int)jObj["MaxHP"];
+                localPet.Name = (string)jObj["Name"];
+                localPet.Refl = (int)jObj["Refl"];
+                localPet.STR = (int)jObj["STR"];
+                localPet.WIS = (int)jObj["WIS"];
+                localPet.Will = (int)jObj["Will"];
+            }
+            catch(Exception ex)
+            {
+                ;
+            }
+
+            return localPet;
+
+            /*var petParts = pet.Split(',');
             foreach (var part in petParts)
             {
                 var stats = part.Split(":");
@@ -75,10 +108,9 @@ namespace ClassLibraryGameDND.Models.OtherModels
                         localPet.Will = int.Parse(stats[1]);
                         break;
                 }
-            }
-            return localPet;
+            }*/
         }
     }
 
-    
+
 }
